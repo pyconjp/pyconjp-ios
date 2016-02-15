2e79d338d2f82f2dcba3e6801b779ab9c7001c45
Yutaro Muta <muta.yutaro@gmail.com>
2016-02-16 01:55:47 +0900

    first commit.

---
+++ PyConJP2016/ViewController/TalksViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TalksViewController.swift
+//  PyConJP2016
+//
+//  Created by Yutaro Muta on 2016/02/16.
+//  Copyright © 2016年 Yutaro Muta. All rights reserved.
+//
+
+import UIKit
+
+class TalksViewController: UIViewController {
+
+    @IBOutlet weak var talksTabBarItem: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
