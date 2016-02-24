8ca49d8ff0184386ba4dca50e6f0aa4dbccfa27c
Yutaro Muta <muta.yutaro@gmail.com>
2016-02-25 08:10:50 +0900

    [modify] color.

--- PyConJP2016/ViewController/Talks/TalksDateViewController.swift
+++ PyConJP2016/ViewController/Talks/TalksDateViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class TalksDateViewController: UIViewController {
     
-    var activeBar: UIView = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: UIScreen.mainScreen().bounds.width, height: 3)))
+    @IBOutlet weak var activeBarView: UIView!
+    var activeBar: UIView = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: UIScreen.mainScreen().bounds.width / 2, height: 3)))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        activeBar.backgroundColor = UIColor.redColor()
+        activeBarView.addSubview(activeBar)
     }
 
     override func didReceiveMemoryWarning() {
