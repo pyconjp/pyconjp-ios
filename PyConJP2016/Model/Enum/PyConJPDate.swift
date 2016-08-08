2e4dcba6370bce44fa7f41f69a37a81f759fe73f
Yutaro Muta <muta.yutaro@gmail.com>
2016-08-08 16:55:52 +0900

    Emit PyConJPDate

--- PyConJP2016/Model/Enum/PyConJPDate.swift
+++ PyConJP2016/Model/Enum/PyConJPDate.swift
@@ -14,20 +14,7 @@ enum PyConJPDate: String {
     case Confarence2 = "2016-09-22"
     case Sprints = "2016-09-23"
     
-//    var date: String {
-//        switch self {
-//        case .Tutorials:
-//            return "2016-09-20"
-//        case .Confarence1:
-//            return "2016-09-21"
-//        case .Confarence2:
-//            return "2016-09-22"
-//        case .Sprints:
-//            return "2016-09-23"
-//        }
-//    }
-    
     static func confarenceDate() -> [PyConJPDate] {
-        return [PyConJPDate.Confarence1, PyConJPDate.Confarence2]
+        return [Confarence1, Confarence2]
     }
 }
