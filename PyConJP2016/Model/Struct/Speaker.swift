59b0e922fc0f637d52a8ce2302bfa97abf9e5e6e
Yutaro Muta <muta.yutaro@gmail.com>
2016-09-08 00:02:11 +0900

    Use Speaker.swift on TalkDetail

--- PyConJP2016/Model/Struct/Speaker.swift
+++ PyConJP2016/Model/Struct/Speaker.swift
@@ -10,31 +10,25 @@ import UIKit
 
 struct Speaker {
     
-    let id: Int
     let name: String
     let imageURL: String
     let twitterName: String
-    let githubName: String
     
 }
 
 extension Speaker {
     
     init(dictionary: [String : AnyObject]) {
-        id = dictionary["id"] as? Int ?? 0
         name = dictionary["name"] as? String ?? ""
-        imageURL = dictionary["image_url"] as? String ?? ""
+        imageURL = dictionary["image_uri"] as? String ?? ""
         twitterName = dictionary["twitter"] as? String ?? ""
-        githubName = dictionary["github"] as? String ?? ""
     }
     
     init?(dictionary: [String : AnyObject]?) {
         guard let dictionary = dictionary else { return nil }
-        id = dictionary["id"] as? Int ?? 0
         name = dictionary["name"] as? String ?? ""
-        imageURL = dictionary["image_url"] as? String ?? ""
+        imageURL = dictionary["image_uri"] as? String ?? ""
         twitterName = dictionary["twitter"] as? String ?? ""
-        githubName = dictionary["github"] as? String ?? ""
     }
     
 }
