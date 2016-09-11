a5bfda51146a90975d0738dc004f0012269a5966
Yutaro Muta <muta.yutaro@gmail.com>
2016-09-11 14:41:29 +0900

    Add StaffTableViewCell

---
+++ PyConJP2016/Model/Struct/Team.swift
@@ -0,0 +1,20 @@
+//
+//  Team.swift
+//  PyConJP2016
+//
+//  Created by Yutaro Muta on 9/10/16.
+//  Copyright © 2016 PyCon JP. All rights reserved.
+//
+
+import UIKit
+
+struct Team {
+
+    let name: String
+    var staffs: [Staff]
+    
+}
+
+extension Team {
+    
+}
