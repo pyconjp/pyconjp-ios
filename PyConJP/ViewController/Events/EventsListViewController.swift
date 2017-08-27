95ff356d5440791030f25d847bf2c33ece53bd54
Yutaro Muta <muta.yutaro@gmail.com>
2017-08-28 01:37:58 +0900

    Fix event list

--- PyConJP/ViewController/Events/EventsListViewController.swift
+++ PyConJP/ViewController/Events/EventsListViewController.swift
@@ -11,56 +11,59 @@ import SafariServices
 import WebAPIFramework
 
 class EventsListViewController: UITableViewController {
+
+    private let sections: [Section] = [.tutorialsDay, .conferenseDay, .sprintsDay]
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if let indexPath = tableView.indexPathForSelectedRow {
-            tableView.deselectRow(at: indexPath, animated: true)
-        }
+    // MARK: - Table View Controller DataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].rows.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].headerTitle
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
+        cell.textLabel?.text  = sections[indexPath.section].rows[indexPath.row].title
+        return cell
     }
     
     // MARK: - Table View Controller Delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let sectionType = SectionType(rawValue: (indexPath as NSIndexPath).section) else { return }
-        let rowType = sectionType.rows[(indexPath as NSIndexPath).row]
-        let safariViewController = SFSafariViewController(url: URL(string: rowType.url)!)
+        let row = sections[indexPath.section].rows[indexPath.row]
+        let safariViewController = SFSafariViewController(url: URL(string: row.url)!)
         self.present(safariViewController, animated: true, completion: nil)
     }
     
-    private enum SectionType: Int, CustomStringConvertible {
+     enum Section {
         case tutorialsDay
         case conferenseDay
         case sprintsDay
         
-        var description: String {
+        var headerTitle: String {
             switch self {
             case .tutorialsDay:
-                return ""
+                return "2017-09-07"
             case .conferenseDay:
-                return ""
+                return "2017-09-08, 09"
             case .sprintsDay:
-                return ""
+                return "2017-09-10"
             }
         }
         
-//        var rows: [RowType] {
-//            switch self {
-//            case .tutorialsDay:
-//                return [.tutorial]
-//            case .conferenseDay:
-//                return [.keynote, .invitedLecture, .posters, .lightningTalks, .youthCoderWorkshop, .beginnerSession, .committeeMeeting, .communityBooth, .jobFair, .productFair, .openSpace]
-//            case .sprintsDay:
-//                return [.sprints]
-//            }
-//        }
-        var rows: [RowType] {
+        var rows: [Row] {
             switch self {
             case .tutorialsDay:
-                return [.tutorial]
+                return [.tutorials]
             case .conferenseDay:
-                return [.keynote, .posters, .lightningTalks, .youthCoderWorkshop, .committeeMeeting, .communityBooth, .jobFair, .openSpace]
+                return [.keynote, .posters, .lightningTalks, .youthCoderWorkshop, .committeeMeeting, .communityBooth, .jobFair, .openSpace, .mediaMeeting]
             case .sprintsDay:
                 return [.sprints]
             }
@@ -68,75 +71,66 @@ class EventsListViewController: UITableViewController {
         
     }
     
-    private enum RowType: CustomStringConvertible {
-        case tutorial
+    enum Row {
+        case tutorials
         
         case keynote
-//        case invitedLecture
         case posters
         case lightningTalks
         case youthCoderWorkshop
-//        case beginnerSession
         case committeeMeeting
         case communityBooth
         case jobFair
-//        case productFair
         case openSpace
         case mediaMeeting
         
         case sprints
         
-        var description: String {
+        var title: String {
             switch self {
-            case .tutorial:
-                return WebConfig.baseURL + "events/tutorial/"
+            case .tutorials:
+                return NSLocalizedString("tutorials", tableName: "EventList", comment: "")
             case .keynote:
-                return WebConfig.baseURL + "talks/keynote/"
+                return NSLocalizedString("keynote", tableName: "EventList", comment: "")
             case .posters:
-                return WebConfig.baseURL + "schedule/posters/list/"
+                return NSLocalizedString("posters", tableName: "EventList", comment: "")
             case .lightningTalks:
-                return WebConfig.baseURL + "events/lightning-talks/"
+                return NSLocalizedString("lightningTalks", tableName: "EventList", comment: "")
             case .youthCoderWorkshop:
-                return WebConfig.baseURL + "events/youth-ws/"
+                return NSLocalizedString("youthCoderWorkshop", tableName: "EventList", comment: "")
             case .committeeMeeting:
-                return WebConfig.baseURL + "events/committee-meeting/"
+                return NSLocalizedString("committeeMeeting", tableName: "EventList", comment: "")
             case .communityBooth:
-                return WebConfig.baseURL + "events/community/"
+                return NSLocalizedString("communityBooth", tableName: "EventList", comment: "")
             case .jobFair:
-                return WebConfig.baseURL + "events/jobsfair/"
+                return NSLocalizedString("jobFair", tableName: "EventList", comment: "")
             case .openSpace:
-                return WebConfig.baseURL + "events/open-space/"
+                return NSLocalizedString("openSpace", tableName: "EventList", comment: "")
             case .mediaMeeting:
-                return WebConfig.baseURL + "events/media-meeting/"
+                return NSLocalizedString("mediaMeeting", tableName: "EventList", comment: "")
             case .sprints:
-                return WebConfig.baseURL + "events/sprint/"
+                return NSLocalizedString("sprints", tableName: "EventList", comment: "")
             }
         }
         
         var url: String {
             switch self {
-            case .tutorial:
+            case .tutorials:
                 return WebConfig.baseURL + "events/tutorial/"
             case .keynote:
                 return WebConfig.baseURL + "talks/keynote/"
-//            case .invitedLecture: 
-//                return WebConfig.baseURL + "talks/invited-speaker/"
             case .posters:
                 return WebConfig.baseURL + "schedule/posters/list/"
             case .lightningTalks: 
                 return WebConfig.baseURL + "events/lightning-talks/"
             case .youthCoderWorkshop:
                 return WebConfig.baseURL + "events/youth-ws/"
-//            case .beginnerSession: 
-//                return WebConfig.baseURL + "events/beginners/"
             case .committeeMeeting:
                 return WebConfig.baseURL + "events/committee-meeting/"
             case .communityBooth:
                 return WebConfig.baseURL + "events/community/"
             case .jobFair:
                 return WebConfig.baseURL + "events/jobsfair/"
-//            case .productFair: 
-//                return WebConfig.baseURL + "productfair/"
             case .openSpace:
                 return WebConfig.baseURL + "events/open-space/"
             case .mediaMeeting:
