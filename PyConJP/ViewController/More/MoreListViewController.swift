b751f233f0896291c386f8381eff8941db15020b
Yutaro Muta <muta.yutaro@gmail.com>
2017-09-07 00:43:15 +0900

    Add SurveyURLAPIRequest

--- PyConJP/ViewController/More/MoreListViewController.swift
+++ PyConJP/ViewController/More/MoreListViewController.swift
@@ -14,6 +14,26 @@ class MoreListViewController: UITableViewController {
     
     private let sections: [Section] = Section.sections
     
+    // MARK: - Table View Controller DataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].rows.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MoreCell", for: indexPath)
+        cell.textLabel?.text = sections[indexPath.section].rows[indexPath.row].title
+        return cell
+    }
+    
     // MARK: - Table View Controller Delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -57,6 +77,17 @@ class MoreListViewController: UITableViewController {
             }
         }
         
+        var title: String? {
+            switch self {
+            case .about:
+                return nil
+            case .venue:
+                return NSLocalizedString("venue", tableName: "MoreList", comment: "")
+            case .application:
+                return NSLocalizedString("application", tableName: "MoreList", comment: "")
+            }
+        }
+        
     }
     
     private enum Row: MailURLSchemeProtocol {
@@ -74,32 +105,71 @@ class MoreListViewController: UITableViewController {
         case license
         case feedback
         
+        var title: String {
+            switch self {
+            case .participantsInformation:
+                return NSLocalizedString("participantsInformation", tableName: "MoreList", comment: "")
+            case .whatsPyConJP:
+                return NSLocalizedString("whatsPyConJP", tableName: "MoreList", comment: "")
+            case .codeOfConduct:
+                return NSLocalizedString("codeOfConduct", tableName: "MoreList", comment: "")
+            case .summary:
+                return NSLocalizedString("summary", tableName: "MoreList", comment: "")
+            case .sponsor:
+                return NSLocalizedString("sponsor", tableName: "MoreList", comment: "")
+            case .staffList:
+                return NSLocalizedString("staffList", tableName: "MoreList", comment: "")
+            case .conferenceMap:
+                return NSLocalizedString("conferenceMap", tableName: "MoreList", comment: "")
+            case .sprintMap:
+                return NSLocalizedString("sprintMap", tableName: "MoreList", comment: "")
+            case .repository:
+                return NSLocalizedString("repository", tableName: "MoreList", comment: "")
+            case .license:
+                return NSLocalizedString("license", tableName: "MoreList", comment: "")
+            case .feedback:
+                return NSLocalizedString("feedback", tableName: "MoreList", comment: "")
+            }
+        }
+        
         var identifier: String? {
             switch self {
-            case .whatsPyConJP: return "WhatsPyConJPViewController"
-            case .codeOfConduct: return "CodeOfConductViewController"
-            case .summary: return "SummaryViewController"
-            case .license: return "LicenseViewController"
-            case .staffList: return "StaffListViewController"
-            default: return nil
+            case .whatsPyConJP:
+                return "WhatsPyConJPViewController"
+            case .codeOfConduct:
+                return "CodeOfConductViewController"
+            case .summary:
+                return "SummaryViewController"
+            case .license:
+                return "LicenseViewController"
+            case .staffList:
+                return "StaffListViewController"
+            default:
+                return nil
             }
         }
         
         var url: URL? {
             switch self {
-            case .participantsInformation: return URL(string: WebConfig.baseURL + "participants/")
-            case .sponsor: return URL(string: WebConfig.baseURL + "sponsors/")
-            case .repository: return URL(string: "https://github.com/pyconjp/pyconjp-ios")
-            default: return nil
+            case .participantsInformation:
+                return URL(string: WebConfig.baseURL + "participants/")
+            case .sponsor:
+                return URL(string: WebConfig.baseURL + "sponsors/")
+            case .repository:
+                return URL(string: "https://github.com/pyconjp/pyconjp-ios")
+            default:
+                return nil
             }
         }
         
         var urlSheme: URL? {
             switch self {
-            case .feedback: return mailURLScheme(to: PCJConfig.mailAddress,
-                                                 subject: "Feedback for PyCon JP 2017 App",
-                                                 body: String(format: "iOS version: %@\nDevice Model: %@\nReply-to:\n\nFeedback:", arguments: [UIDevice.current.systemVersion, UIDevice.current.modelType]))
-            default: return nil
+            case .feedback:
+                return mailURLScheme(to: PCJConfig.mailAddress,
+                                     subject: "Feedback for PyCon JP 2017 App",
+                                     body: String(format: "iOS version: %@\nDevice Model: %@\nReply-to:\n\nFeedback:", arguments: [UIDevice.current.systemVersion, UIDevice.current.modelType]))
+            default:
+                return nil
             }
         }
         
